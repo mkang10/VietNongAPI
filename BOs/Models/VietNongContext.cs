17b22d1be44c075266c786e04b622e08fbc940bb
mkang10 <144370115+mkang10@users.noreply.github.com>
2024-11-12 10:56:29 +0700

    db online

--- BOs/Models/VietNongContext.cs
+++ BOs/Models/VietNongContext.cs
@@ -38,11 +38,6 @@ public partial class VietNongContext : DbContext
     public virtual DbSet<User> Users { get; set; }
 
     public virtual DbSet<WeatherForecast> WeatherForecasts { get; set; }
-
-//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-//        => optionsBuilder.UseSqlServer("Server=LAPTOP-FEOOS2UC;uid=sa;pwd=123;database=VietNong;TrustServerCertificate=True");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<AgriculturalNews>(entity =>
